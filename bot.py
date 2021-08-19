02ce4d1f36ee12f9c963ec116c2ebf5fd699ea78
ridhwan-aziz <qitungagap@gmail.com>
2021-08-20 01:04:30 +0700

    Menambahkan waktu respon pada perintah ping pong

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import telebot, re, os, requests
 from googletrans import Translator
+from datetime import datetime
 
 #Setup Environment
 UD_API = "http://api.urbandictionary.com/v0/define?term={}"
 i = re.IGNORECASE
 reg = re.search
 res = re.split
+t = telebot.TeleBot(os.environ.get('TOKEN'), parse_mode="HTML")
 tr = Translator().translate
 bot = telebot.TeleBot(os.environ.get('TOKEN'), parse_mode="HTML")
 languages_code = ['auto', 'af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-cn', 'zh-tw', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
@@ -16,12 +18,18 @@ bantu = "Bantuan tersedia:\n\n<code>ping, translate</code>\n\nKamu bisa mengguna
 
 print("---BOT STARTED---")
 
+# function handlers
 def start_handler(msg):
     if msg.chat.type != "private":
-        bot.reply_to(msg, "Ya, PM saya aja kalo pengen liat perintah yang tersedia")
+        return 'bot.reply_to(msg, "Ya, PM saya aja kalo pengen liat perintah yang tersedia")'
 
 def ping_handler(msg):
-    bot.reply_to(msg, "<b>PONG!</b>")
+    waktu_awal = datetime.utcfromtimestamp(int(msg.date))
+    pesan = bot.reply_to(msg, "<b>PONG!</b>")
+    waktu_akhir = datetime.now()
+    total_waktu = waktu_akhir - waktu_awal
+    hasil = "<b>PONG!</b>\nWaktu respon <code>" + str(total_waktu.total_seconds()) + "</code> detik"
+    bot.edit_message_text(hasil, msg.chat.id, pesan.message_id)
 
 def translate_handler(msg, rex):
     tujuan = rex[3]
--- push.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/sh
-
-git add . || echo Failed while git add
-echo Changes?
-read changes
-git commit -m "$changes" || echo Failed while committing changes
-git push heroku master || echo Failed while push to heroku
