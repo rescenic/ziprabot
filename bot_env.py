932306c7bb0eb62155b6e73dbb056c1f321e9cb2
Aziz Ridhwan Pratama <69745848+ridhwan-aziz@users.noreply.github.com>
2021-08-23 10:14:35 +0700

    Reclean

--- bot_env.py
+++ bot_env.py
@@ -10,9 +10,6 @@ reg = re.search
 res = re.split
 tr = Translator().translate
 languages_code = ['auto', 'af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-cn', 'zh-tw', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
-if LOG_CHANNEL == None:
-    raise ValueError("LOG_CHANNEL isn't specified!")
 
 #Strings
 adminlisterror = "Tidak bisa menampilkan Admin di private chat😡"
